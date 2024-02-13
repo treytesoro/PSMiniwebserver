464bc18b7fec4f0d0a9232bc324142f6980660b4
Tracie Tesoro <tgtesoro@sentara.com>
2024-02-13 09:14:48 -0500

    Added better sample index

--- example.ps1
+++ example.ps1
@@ -10,7 +10,13 @@ if(!$relaunched) {
 
 $host.UI.RawUI.WindowTitle = "Mini Web Server Example";
 
+# First we must import the miniserver module
 Import-Module .\miniserver.psm1 -Force;
+
+########################################################################################
+# Define scriptblocks for our routes.
+# Route scriptblocks will receive the current request and response objects when called.
+########################################################################################
 
 $index_Route = {
     param(
@@ -23,6 +29,31 @@ $index_Route = {
     $Response.StatusCode = 200;
     $Response.ContentLength64 = $output.Length;
     $Response.ContentType = "text/html";
+
+    # Write index content to outputstream
+    [System.IO.StreamWriter] $sw = [System.IO.StreamWriter]::new($Response.OutputStream);
+    $sw.Write($output);
+    $sw.Close();
+}
+
+$examplejson_Route = {
+    param(
+        $Request,
+        $Response
+    )
+
+    $exampledatahash = @{
+        "firstname"="John";
+        "lastname"="Doe";
+    };
+
+    $output = ConvertTo-Json -InputObject $exampledatahash;
+
+    $Response.StatusCode = 200;
+    $Response.ContentLength64 = $output.Length;
+    $Response.ContentType = "application/Json";
+
+    # Write JSON data to outputstream
     [System.IO.StreamWriter] $sw = [System.IO.StreamWriter]::new($Response.OutputStream);
     $sw.Write($output);
     $sw.Close();
@@ -30,6 +61,10 @@ $index_Route = {
 
 # Register web server routes
 Set-MiniServerRoute -RoutePath "/" -ScriptBlock $index_Route;
+Set-MiniServerRoute -RoutePath "/examplejson" -ScriptBlock $examplejson_Route;
 
 # Start the web server
 Start-MiniServer -Hostname "localhost" -Port "9797";
+
+# Open url in default browser
+Start-Process "http://localhost:9797"
